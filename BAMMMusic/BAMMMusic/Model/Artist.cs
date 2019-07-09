b723cf7cdfcace3edc622f17f7c93b02bc4578aa
Bridget Klingler <bridget.klingler@gmail.com>
2019-07-09 10:48:05 -0400

    structure for mvc models, added artist model properties, added context, added test class

---
+++ BAMMMusic/BAMMMusic/Model/Artist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BAMMMusic.Model
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Age { get; set; }
+        public string Hometown { get; set; }
+
+        public virtual List<Album> Albums { get; set; }
+    }
+}
