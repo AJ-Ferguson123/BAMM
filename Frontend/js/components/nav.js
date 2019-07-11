012d327c670a48ad2cacd69dc78a538cd9ebdae3
AJFerguson123 <roverrecovery1@gmail.com>
2019-07-11 10:25:33 -0400

    added nav file

--- Frontend/js/components/nav.js
+++ Frontend/js/components/nav.js
@@ -1,3 +1,17 @@
-export default function Home(){
-    return 'This is Homepage'
+import Home from './nav';
+
+pageBuild();
+
+function pageBuild(){
+    home();
+}
+
+function home(){
+const body = document.getElementById('body');
+const home = document.getElementById('nav_home');
+
+home.addEventListener('click', function(){
+    body.innerHTML = Home();
+});
+
 }
