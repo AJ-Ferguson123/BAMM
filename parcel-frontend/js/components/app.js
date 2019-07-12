293e298317f1490d4e3fde9835055b70a4c285e3
AJFerguson123 <roverrecovery1@gmail.com>
2019-07-12 07:18:53 -0400

    added navValues in app.js file

--- parcel-frontend/js/components/app.js
+++ parcel-frontend/js/components/app.js
@@ -1,11 +1,12 @@
 import Home from './nav';
-
+import Values from './nav';
 
 pageBuild();
 
 function pageBuild(){
    
     home();
+    navValues();
 }
 
 function home(){
@@ -14,6 +15,15 @@ function home(){
 
     home.addEventListener('click', function(){
         body.innerHTML = Home();
-    });
-    
+    });    
+}
+
+function navValues(){
+    const valuesButton  = document.querySelector('nav_values');
+    valuesButton.addEventListener('click', function(){
+      apiActions.getRequest('http://127.0.0.1:5500/', values =>{
+          document.querySelector('#app').innerHTML = Values(values);
+      })
+
+    })
 }
