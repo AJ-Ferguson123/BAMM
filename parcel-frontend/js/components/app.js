a5da11fdde73892f1083ed4103fac33b1f8d885f
Bridget Klingler <bridget.klingler@gmail.com>
2019-07-16 13:24:38 -0400

    moved the body tag up in the html:

--- parcel-frontend/js/components/app.js
+++ parcel-frontend/js/components/app.js
@@ -12,20 +12,40 @@ function pageBuild(){
 }
 
 function home(){
-    const body = document.getElementById('body');
+    const body = document.getElementById('root');
     const home = document.getElementById('nav_home');
 
     home.addEventListener('click', function(){
         body.innerHTML = Home();
     });
-    
 }
+
 function artists(){
     const artist = document.getElementById('nav_Artists');
     artist.addEventListener('click', function(){
-        apiActions.getRequest('https://localhost:44358/api/artist', artists =>{
-            document.getElementById('body').innerHTML= Artists(artists)
-            
+        apiActions.getRequest('https://localhost:44358/api/artists', artists =>{
+            console.log(artists)
+            document.getElementById('root').innerHTML= Artists(artists)
         })
     })
+
+    
+
+    document.getElementById('root').addEventListener("click", function(){
+        if(event.target.classList('add-artist_submit')){
+            const artistName = event.target.parentElement.querySelector('.add-artist_artistname').value;
+            const data ={
+                id: 0,
+                ArtistName: artistName
+            };
+            apiActions.postRequest(
+                "https://localhost:44358/api/artists",
+                data,
+                artists => {
+                  document.querySelector("#root").innerHTML = Artists(artists);
+                }
+              );
+        }
+    })
 }
+
--- parcel-frontend/js/components/artists.js
+++ parcel-frontend/js/components/artists.js
@@ -1,14 +1,20 @@
 export default function Artists(artists){
     return`
         <ul>
-    ${artists.map(artist =>{
-     return`
+      ${artists.map(artist =>{
+       return`
           <li>
-            <h3>${artist}</h3>
+            <h3>${artist.artistName}</h3>
+            <h3>${artist.hometown}</h3>
           </li>
   `
   
     })}
         </ul>
-  `
+
+        <section class="add-artist">
+          <input class="add-artist_artistname" type="text" placeholder="Add an Artist!">
+          <button class="add-artist_submit">Submit</button>
+        </section>
+          `;
   }
