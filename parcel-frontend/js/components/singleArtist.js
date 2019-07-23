ef8d4015d0a4fa222d79483b9b55b1608bdb1407
mega24cde <megan.denman@gmail.com>
2019-07-23 09:55:59 -0400

    singeArtist

---
+++ parcel-frontend/js/components/singleArtist.js
@@ -0,0 +1,24 @@
+export function SingleArtist(singleArtist){
+    return`
+    <ul>
+     /* <h3>${singleArtist.artistName}</h3>
+         <h3>${singleArtist.hometown}</h3>
+         <h3>${singleArtist.age}</h3>
+        <h3>${singleArtist.artistImage}</h3>
+         <input class='single-artist_id' type='hidden' value="${singleArtist.artistId}"/> */
+
+        ${singleArtist.map(album =>{
+            return`
+                <ul>
+               <li>
+                 <h3>${album.albumTitle}</h3>
+                 <h3>${album.albumImage}</h3>
+                 <input class='album_id' type='hidden' value="${album.albumId}"/>
+                 <button class='albumId_submit button'>Choose Album</button>
+                 </li>`
+                 
+                }).join("")}
+                    </ul>
+                    `;
+                  
+                }
