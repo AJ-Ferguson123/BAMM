70924c363ec48fff7f6401b1f80c4e303f787bd2
Mark Palmer <mark.palmer@itsalllive.com>
2019-07-25 13:43:18 -0400

    modified style sheet for home

--- parcel-frontend/js/components/Home.js
+++ parcel-frontend/js/components/Home.js
@@ -1,8 +1,9 @@
 import princeMusician from '~/images/princeMusician.jpg/';
 
+
 export default function Home(){
     return`
-    <h2>Check out the site we created</h2>
+        <h2>BAMM Music Site</h2>
         <p>We wanted to create a interactive way for people to view artist's and album's. This site allows for people to read reviews, as well as create reviews for the current artist's. The site also allows for people to add artist's to the data base themselves. Obviously the review's and artist's that are added will take some time to post due to our admin's having to review the content before being published.</p>
         
         <img src="${princeMusician}">`
